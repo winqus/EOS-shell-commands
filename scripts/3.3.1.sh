22d0fb9be858e3f35ac209d6132362e3114a2332
winqus <67459222+winqus@users.noreply.github.com>
2025-02-12 17:19:36 +0200

    Fix module name 3.3.1.sh

--- scripts/3.3.1.sh
+++ scripts/3.3.1.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt-get install cifs-utils smbclient libldb2 libsmbclient libwbclient0 python3-gpg python3-ldb python3-samba python3-tdb samba-common-bin samba-dsdbmodules samba-vfs-modules samba samba-libs samba-common -y 
+sudo apt-get install cifs-utils smbclient libldb2 libsmbclient libwbclient0 python3-gpg python3-ldb python3-samba python3-tdb samba-common-bin samba-dsdb-modules samba-vfs-modules samba samba-libs samba-common -y
